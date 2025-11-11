3ad8d8e08358505eacbf9af9175c3cc5948660a4
Richard <m.desanta@tutamail.com>
2025-11-12 02:08:52 +0300

    titanic.py

--- titanic.py
+++ titanic.py
@@ -1,20 +1,19 @@
 import matplotlib.pyplot as plt
 import pandas
-
-f = pandas.read_parquet('titanic.parquet')    #открываем исходный файл
-f.to_csv('titanic.csv')    #преобразуем его в cvs
-
-f_1 = pd.read_csv('titanic.csv')    #создаем файл 
-z = file_1.groupby(['Pclass', 'Survived']).size().unstack(fill_value=0)
-
-s = a.div(a.sum(axis=1), axis=0) * 100 
-s.plot(kind='bar', stacked=True)
-
+# ------------------------------------------------------
+file = pandas.read_parquet('titanic.parquet')    #открываем исходный файл
+file.to_csv('titanic.csv')    #преобразуем его в cvs
+# ------------------------------------------------------
+file_1 = pd.read_csv('titanic.csv')    #создаем файл 
+a = file_1.groupby(['Pclass', 'Survived']).size().unstack(fill_value=0)
+# ------------------------------------------------------
+surv = a.div(a.sum(axis=1), axis=0) * 100 
+surv.plot(kind='bar', stacked=True)
+# ------------------------------------------------------
 plt.title('Выживаемость пассажиров от класса')    #задаем данные гистограммы и оси
 plt.xlabel('Класс') 
-plt.ylabel('% выживаемости') 
+plt.ylabel('Процент выживаемости') 
 plt.xticks(rotation=0)
 plt.legend(['Не выжил', 'Выжил']) 
 plt.tight_layout()
-
 plt.show()    #показ гистограммы 
