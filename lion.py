a4ab3978d7ec9363f492a8f80e46c2d455af8199
Richard <m.desanta@tutamail.com>
2025-11-11 23:44:56 +0300

    lion.py

--- lion.py
+++ lion.py
@@ -1,16 +1,41 @@
-import matplotlib    #импортируем библиотеки
+import matplotlib.pyplot as plt
 import docx
-i = str(input())    #элемент, который должна найти программа
-words = {}
 
-f = open('lion.docx').read()    #открывает и читает файл
-doc = docx.Document(f)    #программа читает текст их файла
+doc = docx.Document('lion.docx')    #программа открывает документ
+txt = []
+letters = {}
 
-txt = f.split()
-for i in txt:    #
-    if i in words:
-        words[i] += 1
-    else:
-        words.update({i:1})
+w = str(input(''))
+l = str(input(''))
 
-print(words)            
+for pr in doc.paragraphs:    
+    txt.append(pr.txt)
+
+txt = txt.lower()
+total_words = txt.count(w)
+p_1 = ((total_words / len(txt))*100)
+# ------------------------------------------------------------------------
+for i in txt:    #цикл для нахождения букв в тексте
+    for z in i:
+        if z in l:
+            l[z] += 1
+        
+        else:
+            l.update({z:1})
+# ------------------------------------------------------------------------
+keys = list(l.keys())    #создаем оси для графика
+values = list(l.values())
+plt.bar(keys, values)     #создаем график
+# ------------------------------------------------------------------------
+plt.xlabel("Буквы")    
+plt.ylabel("Количество")
+plt.title("Гистограмма количества букв")
+plt.show()    #показ графика
+# ------------------------------------------------------------------------
+table = docx.add_table(rows = 2, cols = 3)    #создаем таблицу
+table.cell(0, 0).txt = "Слово"
+table.cell(0, 1).txt = "Частота встречи, раз"
+table.cell(0, 2).txt = "Частота встречи в процентах"
+table.cell(1, 0).txt = str(w)
+table.cell(1, 1).txt = str(total_words)
+table.cell(1, 2).txt = str(p_1)
