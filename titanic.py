abfdbe944f260c39e1c6edc94e21691c0f45c629
Richard <m.desanta@tutamail.com>
2025-11-11 23:54:38 +0300

    titanic.py

--- titanic.py
+++ titanic.py
@@ -1,15 +1,14 @@
 import matplotlib.pyplot as plt
 import pandas
 
-file = pandas.read_parquet('titanic.parquet')    #открываем исходный файл
-file.to_csv('titanic.csv')    #преобразуем его в cvs
+f = pandas.read_parquet('titanic.parquet')    #открываем исходный файл
+f.to_csv('titanic.csv')    #преобразуем его в cvs
 
+f_1 = pd.read_csv('titanic.csv')    #создаем файл 
+z = file_1.groupby(['Pclass', 'Survived']).size().unstack(fill_value=0)
 
-file_1 = pd.read_csv('titanic.csv')    #создаем файл 
-a = file_1.groupby(['Pclass', 'Survived']).size().unstack(fill_value=0)
-
-surv = a.div(a.sum(axis=1), axis=0) * 100 
-surv.plot(kind='bar', stacked=True)
+s = a.div(a.sum(axis=1), axis=0) * 100 
+s.plot(kind='bar', stacked=True)
 
 plt.title('Выживаемость пассажиров от класса')    #задаем данные гистограммы и оси
 plt.xlabel('Класс') 
@@ -18,4 +17,4 @@ plt.xticks(rotation=0)
 plt.legend(['Не выжил', 'Выжил']) 
 plt.tight_layout()
 
-plt.show()    #поках гистограммы 
+plt.show()    #показ гистограммы 
