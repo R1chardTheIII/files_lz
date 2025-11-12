bc3f8ec3244b3dd26650ad00f340fe5c85e4bc1d
Richard <m.desanta@tutamail.com>
2025-11-12 19:17:31 +0300

    titanic.py

--- titanic.py
+++ titanic.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
-import pandas
-# ------------------------------------------------------
-file = pandas.read_parquet('titanic.parquet')    #открываем исходный файл
-file.to_csv('titanic.csv')    #преобразуем его в cvs
-# ------------------------------------------------------
-file_1 = pd.read_csv('titanic.csv')    #создаем файл 
-a = file_1.groupby(['Pclass', 'Survived']).size().unstack(fill_value=0)
-# ------------------------------------------------------
-surv = a.div(a.sum(axis=1), axis=0) * 100 
+import pandas as pd
+# --------------------------------------------------------------
+f = pd.read_parquet('titanic.parquet')    #открываем исходный файл
+f.to_csv('titanic.csv')    #преобразуем его в cvs
+# --------------------------------------------------------------
+f_1 = pd.read_csv('titanic.csv')    #создаем файл 
+z = f_1.groupby(['Pclass', 'Survived']).size().unstack(fill_value=0)
+# --------------------------------------------------------------
+surv = z.div(z.sum(axis=1), axis=0) * 100 
 surv.plot(kind='bar', stacked=True)
-# ------------------------------------------------------
+# --------------------------------------------------------------
 plt.title('Выживаемость пассажиров от класса')    #задаем данные гистограммы и оси
 plt.xlabel('Класс') 
 plt.ylabel('Процент выживаемости') 
